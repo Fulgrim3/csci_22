88f918039db846d0157024330bad439a94dc952c
Donald James Ross <54287731+Fulgrim3@users.noreply.github.com>
2024-01-20 15:30:27 -0500

    fixed some errors

--- sample_problems/Hello.java
+++ sample_problems/Hello.java
@@ -1,5 +1,8 @@
 package sample_problems;
 
 public class Hello {
-    
+    public static void main(String[] args) {
+        String x = "Hello fuck fuck my ass"; 
+        System.out.println(x);
+    }
 }

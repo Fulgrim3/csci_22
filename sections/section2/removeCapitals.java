54dd549816a2240229614cd21d9534a34828a7a6
Donald James Ross <54287731+Fulgrim3@users.noreply.github.com>
2024-02-01 18:34:02 -0500

    fixed a oacjage issue with section2

---
+++ sections/section2/removeCapitals.java
@@ -0,0 +1,17 @@
+package sections.section2;
+
+public static String removeCapitals(String s) {
+    if (s == null) {
+        throw new IllegalArgumentException("string cannot be null");
+    }
+
+    // Base case.
+    if (s.equals("")) {
+        return "";
+    }
+
+    // Recursively process the rest of the string.
+    String removedFromRest = ______________;
+
+    // Now decide what to return and return it.
+}
